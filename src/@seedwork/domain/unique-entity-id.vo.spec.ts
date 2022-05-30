bca1fa612c6cb6114bd323ee971bd2761e759929
Paulo Bezerra <phenriquerb01@gmail.com>
2022-05-29 22:19:33 -0300

    Adicionado testes de category e testes do vo do uuid

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,30 @@
+import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../errors/invalid-uuid-error";
+import {validate as uuidValidate } from 'uuid';
+
+function spyValidateMethod() {
+    return jest.spyOn( UniqueEntityId.prototype as any, 'validate');
+}
+
+describe("UniqueEntityId Unit Tests",() => {
+    /**
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    */
+
+    it("should throw error when uuid id invalid", () => {
+        const validateSpy = spyValidateMethod();
+        expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError());
+        expect(validateSpy).toHaveBeenCalled();
+    });
+
+    it("shold accept a uuid passed in constructor", () => {
+        const validateSpy = spyValidateMethod();
+        const uuid = "de80bff2-9b29-44bf-ad3e-17af1093caff";
+        const vo = new UniqueEntityId(uuid);
+        expect(vo.id).toBe(uuid);
+        expect(validateSpy).toHaveBeenCalled();
+        expect(uuidValidate(vo.id)).toBeTruthy();
+    });
+});
