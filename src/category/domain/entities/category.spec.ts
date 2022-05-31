145be589ba70ee6ca660937e1088fd5d4f280b2c
Paulo Bezerra <phenriquerb01@gmail.com>
2022-05-31 17:19:29 -0300

    Entidade abstrata e metodo update em category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -74,15 +74,18 @@ describe("Category Unit Tests", () => {
         data.forEach(i => {
             const category = new Category(i.props, i.id);
             expect(category.id).not.toBeNull();
-            expect(category.id).toBeInstanceOf(UniqueEntityId);
+            expect(category.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
         });
     });
 
-    test('getter of name prop', () => {
+    test('getter and setter of name prop', () => {
         const category = new Category({
             name : "Movie"
         });
         expect(category.name).toBe("Movie");
+
+        category.name = "other name";
+        expect(category.name).toBe("other name");
     });
 
     test('getter and setter of description prop', () => {
@@ -140,6 +143,33 @@ describe("Category Unit Tests", () => {
             created_at: created_at
         });
         expect(category.created_at).toBe(created_at);
+    });
+
+    it('should active a category', () => {
+        const category = new Category({
+            name : "Filmes",
+            is_active: false
+        });
+        category.activate();
+        expect(category.is_active).toBeTruthy();
+    });
+
+    it('should disable a category', () => {
+        const category = new Category({
+            name : "Filmes",
+            is_active: true
+        });
+        category.deactivate();
+        expect(category.is_active).toBeFalsy();
+    });
+
+    it('should update a category', () => {
+        const category = new Category({
+            name : "Movie"
+        });
+        category.update("Documentary", "some description");
+        expect(category.name).toBe("Documentary");
+        expect(category.description).toBe("some description");
     });
 
 });
