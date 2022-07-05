de5feaccd21e04dd87e6517b6a66441fa257c3e2
Paulo Bezerra <phenriquerb01@gmail.com>
2022-07-05 15:03:08 -0300

    commits atrasados

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,8 @@
 import UniqueEntityId from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
 import Entity from "../../../@seedwork/domain/entity/entity";
-import ValidatorRules from "../../../@seedwork/validators/validator-rules";
+import ValidatorRules from "../../../@seedwork/domain/validators/validator-rules";
+import CategoryValidatorFactory from "../validators/category.validator";
+import {EntityValidationError} from "../../../@seedwork/domain/errors/validation-error";
 
 export type CategoryProperties = {
     name: string;
@@ -27,10 +29,12 @@ export class Category extends Entity<CategoryProperties>{
         this.description = description;
     }
 
-    static validate(props: Omit<CategoryProperties, 'created_at'>) {
-        ValidatorRules.values(props.name, "name").required().string();
-        ValidatorRules.values(props.description, "description").string();
-        ValidatorRules.values(props.is_active, "is_active").boolean();
+    static validate(props: CategoryProperties) {
+        const validator = CategoryValidatorFactory.create();
+        const isValid = validator.validate(props);
+        if(!isValid) {
+            throw new EntityValidationError(validator.errors);
+        }
     }
 
     activate() {
